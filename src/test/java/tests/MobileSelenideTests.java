05ede8997c466654b90f57686f41610d20ee1190
Vadim Buravlev <masta54@mail.ru>
2022-04-23 20:30:53 +0700

    Test v5 commit

--- src/test/java/tests/MobileSelenideTests.java
+++ src/test/java/tests/MobileSelenideTests.java
@@ -1,16 +1,23 @@
 package tests;
 
+import config.SetConfig;
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static helpers.Attach.getSessionId;
 import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
 
+@Tag("selenide")
 public class MobileSelenideTests extends TestBase {
+
 
     @Test
     @DisplayName("The first test for the Wiki app for Android")
